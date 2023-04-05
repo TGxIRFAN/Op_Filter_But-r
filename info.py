4097ca8d1d62aa65dbb6209f54a1320b160492a5
Goutham Josh <97418751+GouthamKL16@users.noreply.github.com>
2023-04-05 22:49:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,6 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Bot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
+DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'im_goutham_josh')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
