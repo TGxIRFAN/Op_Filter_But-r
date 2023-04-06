2e8ada38dc5685c8d037068202e59f5c9eaff119
Goutham Josh <97418751+GouthamKL16@users.noreply.github.com>
2023-04-06 20:58:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,8 @@ from pyrogram import types
 from Script import script
 from plugins import web_server
 from aiohttp import web
+from datetime import date, datetime 
+import pytz
 
 PORT = "8080"
 
@@ -48,6 +50,14 @@ class Bot(Client):
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
         
+        #RESTART TEXT
+        tz = pytz.timezone('Asia/Kolkata')
+        today = date.today()
+        now = datetime.now(tz)
+        time = now.strftime("%H:%M:%S %p")
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
+        
+        #PORT
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
