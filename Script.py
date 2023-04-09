a8728b4b7c97b7dfbc8e829b310d0d414842c591
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2023-04-09 20:03:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,7 +2,7 @@ class script(object):
     START_TXT = """Hɪ {},
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, I ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs ᴀɴᴅ Sᴇʀɪᴇs, Jᴜsᴛ Aᴅᴅ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ💥
 
-ᠰMᴀɪɴᴛᴀɪɴᴇᴅ ʙʏᠰ: <a href='https://telegram.me/im_goutham_josh'>Gᴏᴜᴛʜᴀᴍ¹⁶</a>
+ᠰMᴀɪɴᴛᴀɪɴᴇᴅ ʙʏᠰ: <a href='https://telegram.me/im_goutham_josh'>Gᴏᴜᴛʜᴀᴍ Sᴇʀ</a>
 """
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
