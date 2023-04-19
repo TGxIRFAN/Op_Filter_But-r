d25d06d7d95fcf848d1d45b9561ee66f80b87041
Muhammedshifin <muhammedshifink4@gmail.com>
2023-04-19 11:59:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -77,16 +77,16 @@ async def is_subscribed(bot, query):
                 return False
     else:
         try:
-        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-    except UserNotParticipant:
-        pass
-    except Exception as e:
-        logger.exception(e)
-    else:
-        if user.status != 'kicked':
-            return True
+            user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+        except UserNotParticipant:
+            pass
+        except Exception as e:
+            logger.exception(e)
+        else:
+            if user.status != 'kicked':
+                return True
 
-    return False
+        return False
         
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
